b82f10fcab52097483c23beaa787d1fb5697d235
CamCraig91 <cam.arn.craig@gmail.com>
2026-06-02 18:31:38 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <ClientLayout>
           {children}
-        </ClientLayout>
       </body>
     </html>
   );
