646cad1c22bcb217b89197ef23b47fcb9ce20137
CamCraig91 <cam.arn.craig@gmail.com>
2026-07-03 16:23:31 -0400

    Update page.tsx

--- app/(site)/forms/page.tsx
+++ app/(site)/forms/page.tsx
@@ -19,12 +19,18 @@ export default function FormsPage() {
             </p>
           </div>
 
-          {/* VISUAL */}
-          <div className="hidden md:flex justify-center">
-            <div className="w-80 h-56 bg-white rounded-xl shadow-xl border border-gray-100 flex items-center justify-center">
-              <span className="text-gray-400">Screenshot / Visual</span>
-            </div>
-          </div>
+         {/* VISUAL */}
+    <div className="hidden md:flex justify-center">
+      <div className="rounded-xl shadow-xl border border-gray-100 overflow-hidden">
+        <Image
+          src="/forms.png"
+          alt="forms"
+          width={200}
+          height={120}
+          className="object-cover"
+        />
+      </div>
+    </div>
 
         </div>
       </section>
