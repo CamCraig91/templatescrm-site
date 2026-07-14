81b428bd18f023772b451dd09a0d8f237395ef9d
CamCraig91 <cam.arn.craig@gmail.com>
2026-07-14 10:40:19 -0400

    Update page.tsx

--- app/(site)/inventory/page.tsx
+++ app/(site)/inventory/page.tsx
@@ -14,12 +14,18 @@ export default function InventoryPage() {
             </p>
           </div>
 
-          <div className="hidden md:flex justify-center">
-            <div className="w-80 h-56 bg-white rounded-xl shadow-xl border border-gray-100 flex items-center justify-center">
-              <span className="text-gray-400">Screenshot / Visual</span>
-            </div>
-          </div>
-        </div>
+             {/* VISUAL */}
+    <div className="hidden md:flex justify-center">
+      <div className="rounded-xl shadow-xl overflow-hidden">
+        <Image
+          src="/inventorywhite.png"
+          alt="inventory"
+          width={300}
+          height={160}
+          className="object-cover"
+        />
+      </div>
+    </div>
       </section>
 
       <section className="py-20">
